468074c16aea884d309df5f2fcc9285893f602a4
Sjones352 <sjones@tistatech.com>
2017-12-26 01:28:51 -0500

    added nav bar

--- js/validation.js
+++ js/validation.js
@@ -1,11 +1,44 @@
 "use strict";
 
+$(document).ready(function() {
 
-$(window).on('load',function() {
+	var scrollLink = $('.scroll');
 
-$("#btn").click(function(e){
- var url = 'sandrajonesresume.pdf';
-  e.preventDefault();
- window.location.href = url;
-});
+	scrollLink.click(function(e) {
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 1000);
+	});
+
+	$(window).scroll(function() {
+		var scrollbarLocation = $(this).scrollTop();
+
+		scrollLink.each(function() {
+			var sectionoOffset = $(this.hash).offset().top - 20;
+
+			if (sectionoOffset <= scrollbarLocation) {
+				$(this).parent().addClass('active');
+				$(this).parent().siblings().removeClass('active');
+			}
+		});
+
+	});
+
+	$("#hover").hover(function(e) {
+		$('#hide').html('Live your best life');
+		$(this).parent().addClass("show");
+		$('#hide').fadeIn(2000);
+		$('#hover').hide();
+		$('#hide').delay(2000);
+		$('#hide').fadeOut(500);
+
+	});
+
+	$("#btn").click(function(e) {
+		var url = 'sandrajonesresume.pdf';
+		e.preventDefault();
+		window.location.href = url;
+
+	});
 });
